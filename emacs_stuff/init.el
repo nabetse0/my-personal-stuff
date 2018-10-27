12ae88650777ab047dada8d0efa9a92dd7f8ddc3
Steven Chang <steven.chang@asml.com>
2018-10-27 10:49:58 -0700

    Emacs init.el file so I don't lose it.

---
+++ emacs_stuff/init.el
@@ -0,0 +1,18 @@
+;; Taken from http://cachestocaches.com/2015/8/getting-started-use-package/
+;; Installs use-package in case it's not already installed.
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
